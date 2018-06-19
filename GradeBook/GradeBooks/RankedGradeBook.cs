c76ea5349d9aff2f7d35036c5e2895bad365ba89
BrentWiles <40406543+BrentWiles@users.noreply.github.com>
2018-06-19 16:06:49 -0500

    Create new StandardGradeBook and RankedGradeBook classes
    Created the StandardGradeBook class and updated the GradeBookType to Standard.
    
    Created the RankedGradeBook class and updated the GradeBookType to Ranked.

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,12 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook //Inherits BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name) //call to base constructor
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,12 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook //inherits BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name) //call to base constructor
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
